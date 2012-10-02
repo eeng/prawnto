57b2649eb3946cc0463a637128ffd0adc31246ec
Emmanuel Nicolau <emmanicolau@gmail.com>
2012-10-02 13:55:39 -0300

    agrego la opcion silent_print y las variables de instancia del controller en los dsl templates

--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -5,9 +5,22 @@ module Prawnto
     class Base < (base_class_for_template_handler_required? ? ::ActionView::TemplateHandler : ::Object)
       include ::ActionView::TemplateHandlers::Compilable if template_should_include_compilable?
 
+      SILENT_PRINT_CODE = %{
+        if @prawnto_options[:silent_print]
+          script = <<-EOF
+            if (typeof JSSilentPrint == "undefined")
+              this.print({bUI: true, bSilent: true, bShrinkToFit: false});
+            else
+              JSSilentPrint(this);
+          EOF
+          pdf.add_docopen_js 'Silent', script
+        end
+      }
+
       def self.call(template)
         "_prawnto_compile_setup;" +
         "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" +
+        "#{SILENT_PRINT_CODE}\n" +
         "#{template.source}\n" +
         "pdf.render;"
       end
--- lib/prawnto/template_handlers/dsl.rb
+++ lib/prawnto/template_handlers/dsl.rb
@@ -5,6 +5,11 @@ module Prawnto
         <<-RUBY
           _prawnto_compile_setup(true)
           pdf = Prawn::Document.new(@prawnto_options[:prawn])
+          #{SILENT_PRINT_CODE}
+          pdf.extend ActionView::Helpers::TextHelper
+          controller.instance_variables.each do |ivar| 
+            pdf.instance_variable_set ivar, controller.instance_variable_get(ivar)
+          end
           pdf.instance_eval do
             #{template.source}
           end
